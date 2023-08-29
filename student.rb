f1ad509bf4f0cc33e5133453fcc8fed597f4090e
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-08-29 11:26:19 +0600

    Fix Linters

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 reqire_relative 'person'
 
 class Student < Person
-    def initialize(id, name: 'Unknown', age, classroom, parent_permission: true)
-        super(id, name: name, age: age, parent_permission: parent_permission)
-        @classroom = classroom
-    end
-    
-    def play_hooky
-        '¯\(ツ)/¯'
-    end
+  def initialize(id, age:, classroom:, name: 'Unknown', parent_permission: true)
+    super(id, name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
