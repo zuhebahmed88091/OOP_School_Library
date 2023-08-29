1947010b8bbf035495dfa08917af3acd7b67b967
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-08-29 15:58:01 +0600

    Fix linters

--- capitalized_decorator.rb
+++ capitalized_decorator.rb
@@ -1,8 +1,8 @@
 require_relative 'base_decorator'
 
 class CapitalizeDecorator < Decorator
-    def correct_name
-        original_name = @nameable.correct_name
-        original_name.capitalize
-    end
+  def correct_name
+    original_name = @nameable.correct_name
+    original_name.capitalize
+  end
 end
