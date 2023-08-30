1f34d4565a8336306b272431632984aefea9b53e
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-08-30 13:58:03 +0600

    Add rental and book class and also make relation between person and book via rental

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, age,  name = 'Unknown', parent_permission: true)
-    super(id, name, age, parent_permission)
+  def initialize(age,  name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def classroom=(classroom)
